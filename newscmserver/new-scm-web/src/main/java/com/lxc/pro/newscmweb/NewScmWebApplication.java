b9738216d5ae0f9070d479db01498589aa79df0f
lxc <273011079@qq.com>
2019-01-27 20:37:05 +0800

    上传

---
+++ newscmserver/new-scm-web/src/main/java/com/lxc/pro/newscmweb/NewScmWebApplication.java
@@ -0,0 +1,14 @@
+package com.lxc.pro.newscmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewScmWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewScmWebApplication.class, args);
+    }
+
+}
+
